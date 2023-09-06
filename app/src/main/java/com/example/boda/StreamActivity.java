0ee07f5fd7e41d87c01299b6c6f5b64aa2227564
jin-jae <jinjae.dev@gmail.com>
2023-09-06 15:23:13 +0900

    fix: streaming UI/UX

--- app/src/main/java/com/example/boda/StreamActivity.java
+++ app/src/main/java/com/example/boda/StreamActivity.java
@@ -2,7 +2,6 @@ package com.example.boda;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -15,16 +14,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import com.example.boda.utils.PathUtils;
 import com.example.boda.utils.PermissionUtils;
 import com.pedro.rtplibrary.rtsp.RtspCamera1;
 import com.pedro.rtsp.utils.ConnectCheckerRtsp;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class StreamActivity extends AppCompatActivity
         implements ConnectCheckerRtsp, View.OnClickListener, SurfaceHolder.Callback {
@@ -35,13 +27,12 @@ public class StreamActivity extends AppCompatActivity
     };
 
     public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 200;
+    private final String URL = "rtsp://10.0.2.2:8556/boda";
 
     private RtspCamera1 rtspCamera1;
     private Button button;
-    private Button bRecord;
 
     private String currentDateAndTime = "";
-    private File folder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,14 +43,9 @@ public class StreamActivity extends AppCompatActivity
         if (!PermissionUtils.haveAllPermission(StreamActivity.this, permissionList))
             ActivityCompat.requestPermissions(this, permissionList, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
 
-        folder = PathUtils.getRecordPath();
         SurfaceView surfaceView = findViewById(R.id.surfaceView);
         button = findViewById(R.id.b_start_stop);
         button.setOnClickListener(this);
-        bRecord = findViewById(R.id.b_record);
-        bRecord.setOnClickListener(this);
-        Button switchCamera = findViewById(R.id.switch_camera);
-        switchCamera.setOnClickListener(this);
 
         rtspCamera1 = new RtspCamera1(surfaceView, this);
         rtspCamera1.setReTries(10);
@@ -83,17 +69,9 @@ public class StreamActivity extends AppCompatActivity
             }
 
             if ( !check_result ) {
-                // if permitted
+                // if not permitted
                 finish();
             }
-//            else {
-//                if (PermissionUtils.recheckPermission(this, permissionList)){
-//                    finish();
-//                }
-//                else{
-//                    finish();
-//                }
-//            }
         }
     }
 
@@ -107,15 +85,6 @@ public class StreamActivity extends AppCompatActivity
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && rtspCamera1.isRecording()) {
-            rtspCamera1.stopRecord();
-            PathUtils.updateGallery(this, folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
-            bRecord.setText("녹화시작");
-            Toast.makeText(this,
-                    "file " + currentDateAndTime + ".mp4 saved in " + folder.getAbsolutePath(),
-                    Toast.LENGTH_SHORT).show();
-            currentDateAndTime = "";
-        }
         if (rtspCamera1.isStreaming()) {
             rtspCamera1.stopStream();
             button.setText("시작");
@@ -131,60 +100,14 @@ public class StreamActivity extends AppCompatActivity
                 if (rtspCamera1.isRecording()
                         || rtspCamera1.prepareAudio() && rtspCamera1.prepareVideo()) {
                     button.setText("정지");
-                    rtspCamera1.startStream("rtsp://10.0.2.2:8556/boda");
+                    rtspCamera1.startStream(URL);
                 } else {
-                    Toast.makeText(this, "Error preparing stream, This device cant do it",
+                    Toast.makeText(this, "연결에 오류가 발생하였습니다.",
                             Toast.LENGTH_SHORT).show();
                 }
             } else {
                 button.setText("시작");
                 rtspCamera1.stopStream();
-            }
-        } else if (id == R.id.switch_camera) {
-            try {
-                rtspCamera1.switchCamera();
-            } catch (Exception e) {
-                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-            }
-        } else if (id == R.id.b_record) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                if (!rtspCamera1.isRecording()) {
-                    try {
-                        if (!folder.exists()) {
-                            folder.mkdir();
-                        }
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
-                        currentDateAndTime = sdf.format(new Date());
-                        if (!rtspCamera1.isStreaming()) {
-                            if (rtspCamera1.prepareAudio() && rtspCamera1.prepareVideo()) {
-                                rtspCamera1.startRecord(
-                                        folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
-                                bRecord.setText("녹화시작");
-                                Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(this, "Error preparing stream, This device cant do it",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            rtspCamera1.startRecord(
-                                    folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
-                            bRecord.setText("녹화중지");
-                            Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (IOException e) {
-                        rtspCamera1.stopRecord();
-                        PathUtils.updateGallery(this, folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
-                        bRecord.setText("녹화시작");
-                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    rtspCamera1.stopRecord();
-                    PathUtils.updateGallery(this, folder.getAbsolutePath() + "/" + currentDateAndTime + ".mp4");
-                    bRecord.setText("녹화시작");
-                    Toast.makeText(this,
-                            "file " + currentDateAndTime + ".mp4 saved in " + folder.getAbsolutePath(),
-                            Toast.LENGTH_SHORT).show();
-                }
             }
         }
     }
@@ -207,10 +130,10 @@ public class StreamActivity extends AppCompatActivity
     public void onConnectionFailedRtsp(@NonNull String reason) {
         runOnUiThread(() -> {
             if (rtspCamera1.reTry(5000, reason, null)) {
-                Toast.makeText(StreamActivity.this, "재시도", Toast.LENGTH_SHORT)
+                Toast.makeText(StreamActivity.this, "연결에 실패하여 다시 연결을 시도합니다.", Toast.LENGTH_SHORT)
                         .show();
             } else {
-                Toast.makeText(StreamActivity.this, "연결 실패: " + reason, Toast.LENGTH_SHORT)
+                Toast.makeText(StreamActivity.this, reason, Toast.LENGTH_SHORT)
                         .show();
                 rtspCamera1.stopStream();
                 button.setText("재시도");
@@ -223,12 +146,14 @@ public class StreamActivity extends AppCompatActivity
 
     @Override
     public void onConnectionSuccessRtsp() {
-        runOnUiThread(() -> Toast.makeText(StreamActivity.this, "서버 연결 성공", Toast.LENGTH_SHORT).show());
+        runOnUiThread(() ->
+                Toast.makeText(StreamActivity.this, "서버에 정상적으로 연결되었습니다.", Toast.LENGTH_SHORT).show());
     }
 
     @Override
     public void onDisconnectRtsp() {
-        runOnUiThread(() -> Toast.makeText(StreamActivity.this, "서버 연결 종료", Toast.LENGTH_SHORT).show());
+        runOnUiThread(() ->
+                Toast.makeText(StreamActivity.this, "서버와의 연결이 종료되었습니다.", Toast.LENGTH_SHORT).show());
     }
 
     @Override
